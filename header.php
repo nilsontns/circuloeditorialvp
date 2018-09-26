dedee984d3a6e1e5348926020491c7a316dcbb91
nilson_tns <nilsontns@gmail.com>
2018-09-26 18:42:47 -0400

    agregando keywords y otros cambios en header

--- header.php
+++ header.php
@@ -12,14 +12,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta name="google-site-verification" content="ti-9qPFx3jVw3oKC9mEpK-rZZ2D8JN4N_zGixPn5kM4" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
-	<meta name = "keywords" content="circuloeditorialvp"/>﻿
+	<meta name = "keywords" content="circulo editorial vision progrecista, circuloeditorialvp"/>﻿
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
