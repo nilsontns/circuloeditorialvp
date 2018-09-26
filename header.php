f936442c2b77d7e088cbd2d3a5e681ae7cd8ba26
nilson_tns <nilsontns@gmail.com>
2018-09-26 19:13:18 -0400

    add google meta

--- header.php
+++ header.php
@@ -12,6 +12,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<meta name="google-site-verification" content="ti-9qPFx3jVw3oKC9mEpK-rZZ2D8JN4N_zGixPn5kM4" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
