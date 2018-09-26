7ee1e98f17a445acbb505563b682d206c64b6a47
nilson_tns <nilsontns@gmail.com>
2018-09-26 03:30:37 -0400

    quitando meta de google en header

--- header.php
+++ header.php
@@ -14,7 +14,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="google-site-verification" content="JDXaZwppPx01rY9CeiIxuArY-_3gODHcP1vZIR1MTVc" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
