a56cd54773f481fbf30212513c59ed8e3ff40664
nilson_tns <nilsontns@gmail.com>
2018-09-26 02:17:11 -0400

    correccion en el footer

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<footer class="site-footer" id="colophon">
 
 					<div class="site-info text-center" style="color:white"  >
-							<p>Copyright © 2018 · Circulo Editorial Visión Progrecista (CEVP) </p>
+							<p>Copyright © 2018 · Circulo Editorial Visión Progresista (CEVP) </p>
 							<p> Desarrollado por <b>Nilson Toro</b> &middot;  Desarrollador Web Full Stack &middot; nilsontns@gmail.com &middot; Caracas , Venezuela   </p>
 
 					</div><!-- .site-info -->
